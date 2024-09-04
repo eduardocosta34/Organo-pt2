6f50d4c30035db8b40f23d6358bcdbf7cf2f72fd
Eduardo Costa Santos <106274059+eduardocosta34@users.noreply.github.com>
2024-09-04 16:46:31 -0300

    Add files via upload

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+
+function Banner() {
+  // JSX - parece html mas não é!
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="Banner principal da página" />;
+    </header>
+  );
+}
+
+export default Banner;
